a4c6eb3479e0ec1028de76911fd69b47189c145c
jeglasper <109081362+jeglasper@users.noreply.github.com>
2023-04-07 22:44:08 -0500

    add validation to ensure initials are 1-3 characters

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const {Circle, Triangle, Square} = require('./lib/shapes');
+const { throwError } = require('rxjs');
 
 function svgFileContent ({text,textColor,shape, shapeColor}, shapeContent) {
     return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
@@ -18,6 +19,13 @@ const questions = [
         type: 'input',
         message: 'INITIALS: What are your initials?',
         name: 'text',
+        validate: function(input) {
+            if(input.length > 3 || input.length == 0) {
+                return "Initials must include 1-3 characters"
+            } else {
+                return true;
+            }
+        } 
     }, {
         type: 'input',
         message: 'TEXT COLOR: What color would you like your initials to be (color keyword or hexadecimal number)?',
